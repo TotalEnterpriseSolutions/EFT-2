972f452066654821cbcd3e99c3d5898e61fc9c61
Chris Wilson <chris.wilson@totalenterprisesolutions.co.uk>
2017-11-06 15:43:56 +0000

    upload

---
+++ Test_Sandbox/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TES.Integration.IPSockets;
+
+namespace Test_Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string errorText = "";
+            Base b = new Base("987877898", 23, 5);
+            try
+            {
+                if (b.connect(ref errorText))
+                {
+                    Console.WriteLine("Connected.....");
+                }
+                else
+                {
+                    Console.WriteLine(errorText);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.Read();
+        }
+    }
+}
