045892edfdedf76b11959a17fe6a3f681b9d749d
Chris Wilson <chris.wilson@totalenterprisesolutions.co.uk>
2017-12-18 15:52:27 +0000

    CW 18/12/2017

---
+++ TES.Integration.IPSockets2/StringFunctions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TES.Integration.IPSockets2
+{
+    public class StringFunctions
+    { 
+        public Dictionary<int, string> NvPToDictionary(string v_inputtext, string v_lineseperator, string v_valueseperator)
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            string[] strArray = v_inputtext.Split(v_lineseperator.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            foreach (string str in strArray)
+            {
+                int key = 0;
+                string[] strArray2 = str.Split(v_valueseperator.ToCharArray());
+                key = int.Parse(strArray2[0]);
+                dictionary.Add(key, strArray2[1]);
+            }
+            return dictionary;
+        }
+    }
+}
